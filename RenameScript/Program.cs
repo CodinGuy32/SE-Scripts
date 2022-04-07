28ca203ca9f5697860b03094ecc4a62d00d57e8d
CodinGuy <88629925+CodinGuy32@users.noreply.github.com>
2022-04-07 07:39:50 +0200

    Rename Script Source Added
    ADDED the source code of Rename Script https://steamcommunity.com/sharedfiles/filedetails/?id=2790565268

---
+++ RenameScript/Program.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+
+        public Program() { }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if(argument.Length < 1) 
+            {
+                Echo("Please provide a prefix in the argument text box.");
+                return;
+            }
+
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid == Me.CubeGrid);
+            string prefix = argument + " - ";
+            int count = 0;
+            foreach (IMyTerminalBlock block in blocks)
+            {
+                block.CustomName = prefix + block.DefinitionDisplayNameText;
+                count++;
+            }
+            Echo(count + " Block(s) renamed!");
+        }
+
+    }
+}
